87abbf37f811d877b9e7181d455d8c7bdfb342a6
Isaac de oliveira sales <isaacsalesoliver@gmail.com>
2025-06-04 10:21:04 -0300

    prints e variaveis

---
+++ ghost.py
@@ -0,0 +1,12 @@
+ghost=312
+price=123
+print(ghost + price)
+print(ghost - price)
+print(ghost * price)
+print(ghost / price)
+print(ghost % price)
+nome = "isaac"
+idade= 15
+print("minha idade é", idade)
+print("minha idade é", idade, "e meu nome é", nome)
+
