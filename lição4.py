57e2ea8ab00490f5c8755c07a77ece6568d54122
Isaac de oliveira sales <isaacsalesoliver@gmail.com>
2025-06-09 11:36:15 -0300

    Add files via upload

---
+++ lição4.py
@@ -0,0 +1,8 @@
+Alimento_nome = input("Nome do alimento:") 
+Valor_alimento = float(input("valor do alimento:"))
+
+print(f"-----------------------------")
+print(f"| Alimento: Alimento_nome")
+print(f"| Valor: Valor_alimento")
+print(f"-----------------------------")
+      
