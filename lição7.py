c80a7c10051d616d0ac0b504b065223d57b22c5c
Isaac de oliveira sales <isaacsalesoliver@gmail.com>
2025-06-09 11:47:46 -0300

    Update lição7.py

--- lição7.py
+++ lição7.py
@@ -2,11 +2,11 @@ nome = input("Qual seu nome?:")
 nota_1 = float(input("Qual sua nota?"))
 nota_2 = float(input("segunda nota?"))
 nota_3 = float(input("terceira nota?"))
-media = nota_1 + nota_2 + nota_3/3
+media = (nota_1 + nota_2 + nota_3)/3
 print(media)
 if media >= 7:
-    print("aprovado")
+    print(f"O {nome} aprovado por {media}")
 elif media >= 4:
-    print("recuperação")
+    print(f"O {nome} em recuperação por {media}")
 else:
-    print("seu QI é negativo")
+    print(f"O {nome}, seu QI é negativo, por {media}")
